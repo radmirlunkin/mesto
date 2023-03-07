0613e0c346d50e70cb04412e5ff241cdc11f310e
Radmir Lunkin <evdokimova.angelino4cka@yandex.ru>
2023-03-07 22:25:35 +0300

    new

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+let main = document.querySelector('.main')
+let ProfileEditButton = document.querySelector('.profile__edit-button')
+
+let PopUpOpened = document.querySelector('.popup__opened')
+let PopUpButtonClose = document.querySelector('.popup__close-button')
+let PopUpFieldName = document.querySelector('.popup__input-name')
+let PopUpFieldJob = document.querySelector('.popup__input-job')
+let PopUpSaveButton = document.querySelector('.popup__save-button')
+let ProfileName = document.querySelector('.profile__name')
+let ProfileJob = document.querySelector('.profile__job')
+let formElement = document.querySelector('.popup__form')
+function openPopup() {
+    PopUpOpened.setAttribute('style','display: flex')
+    PopUpFieldName.value = ProfileName.textContent;
+    PopUpFieldJob.value = ProfileJob.textContent;
+}
+
+function closePopup() {
+    PopUpOpened.setAttribute('style','display: none')
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    ProfileName.textContent = PopUpFieldName.value;
+    ProfileJob.textContent = PopUpFieldJob.value;
+    closePopup();
+}
+
+ProfileEditButton.addEventListener('click', openPopup);
+PopUpButtonClose.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
