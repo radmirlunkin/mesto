039deac27cb78d8d5ba90d6a7600a5669ae7e9a1
Radmir Lunkin <evdokimova.angelino4cka@yandex.ru>
2023-03-12 21:23:22 +0300

    new1

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,30 @@
-let main = document.querySelector('.main')
-let ProfileEditButton = document.querySelector('.profile__edit-button')
-
-let PopUpOpened = document.querySelector('.popup_opened')
-let PopUpButtonClose = document.querySelector('.popup__close-button')
-let PopUpFieldName = document.querySelector('.popup__input-name')
-let PopUpFieldJob = document.querySelector('.popup__input-job')
-let PopUpSaveButton = document.querySelector('.popup__save-button')
-let ProfileName = document.querySelector('.profile__name')
-let ProfileJob = document.querySelector('.profile__job')
+let popUp = document.querySelector('.popup')
+let profileEditButton = document.querySelector('.profile__edit-button')
+let popUpOpened = document.querySelector('.popup_opened')
+let popUpButtonClose = document.querySelector('.popup__close-button')
+let popUpFieldName = document.querySelector('.popup__input-name')
+let popUpFieldJob = document.querySelector('.popup__input-job')
+let popUpSaveButton = document.querySelector('.popup__save-button')
+let profileName = document.querySelector('.profile__name')
+let profileJob = document.querySelector('.profile__job')
 let formElement = document.querySelector('.popup__form')
 function openPopup() {
-    PopUpOpened.setAttribute('style','display: flex')
-    PopUpFieldName.value = ProfileName.textContent;
-    PopUpFieldJob.value = ProfileJob.textContent;
+    popUp.classList.add('popup_opened');
+    popUpFieldName.value = profileName.textContent;
+    popUpFieldJob.value = profileJob.textContent;
 }
 
 function closePopup() {
-    PopUpOpened.setAttribute('style','display: none')
+    popUp.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    ProfileName.textContent = PopUpFieldName.value;
-    ProfileJob.textContent = PopUpFieldJob.value;
+    profileName.textContent = popUpFieldName.value;
+    profileJob.textContent = popUpFieldJob.value;
     closePopup();
 }
 
-ProfileEditButton.addEventListener('click', openPopup);
-PopUpButtonClose.addEventListener('click', closePopup);
+profileEditButton.addEventListener('click', openPopup);
+popUpButtonClose.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
